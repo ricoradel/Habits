8fd574fa94db616da834a0ad5cf2228ee0c90244
Ricardo <ricardocoradel@outlook.com>
2023-01-22 22:17:47 -0300

    NLW Seteup Ignite - class project final

---
+++ mobile/src/screens/Habit.tsx
@@ -0,0 +1,126 @@
+import { Alert, ScrollView, Text, View } from "react-native";
+import { useRoute } from '@react-navigation/native'
+import { BackButton } from "../components/BackButton";
+import dayjs from 'dayjs'
+import { parse } from "react-native-svg";
+import { ProgressBar } from "../components/ProgressBar";
+import { CheckBox } from "../components/CheckBox";
+import { useEffect, useState } from "react";
+import { Loading } from "../components/Loading";
+import { api } from "../lib/axios";
+import { generateProgressPercentage } from "../utils/generate-progress-percentage"; 
+import { HabitsEmpty } from "../components/HabitsEmpty";
+import clsx from "clsx";
+
+interface Params {
+  date: string
+}
+
+interface DayInfoProps {
+  completedHabits: string[],
+  possibleHabits: {
+    id: string,
+    title: string,
+  }[]
+}
+
+export function Habit() {
+  const [loading, setLoading] = useState(true)
+  const [dayInfo, setDayInfo] = useState<DayInfoProps | null>(null)
+  const [completedHabits, setCompletedHabits] = useState<string[]>([])
+  const route = useRoute()
+  const { date } = route.params as Params
+  const parsedDate = dayjs(date)
+  const dayOfWeek = parsedDate.format('dddd')
+  const dayAndMonth = parsedDate.format('DD/MM')
+  const isDateInPast = parsedDate.endOf('day').isBefore(new Date())
+  
+  const habitsProgress = dayInfo?.possibleHabits.length 
+  ? generateProgressPercentage(dayInfo.possibleHabits.length, completedHabits.length) 
+  : 0
+
+  async function fetchHabits() {
+    try {
+      setLoading(true)
+      const response = await api.get('/day', { params: { date }})
+      setDayInfo(response.data)
+      setCompletedHabits(response.data.completedHabits)
+    } catch (error) {
+      Alert.alert('Ops', 'Não foi possível carregar as informações dos hábitos') 
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  async function handleToggleHabits(habitId: string) {
+    try {
+      await api.patch(`/habits/${habitId}/toggle`)
+
+      if (completedHabits.includes(habitId)){
+        setCompletedHabits(prevState => prevState.filter(habit => habit !== habitId))
+      } else {
+        setCompletedHabits(prevState => [...prevState, habitId])
+      }
+    } catch (error) {
+      Alert.alert('Ops', 'Não foi possível atualizar o status do hábito')
+    } 
+  }
+
+  useEffect(() => {
+    fetchHabits()
+  }, [])
+
+  if (loading) {
+    return (
+      <Loading />
+    )
+  }
+
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
+
+        <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
+          {dayOfWeek}
+        </Text>
+
+        <Text className="text-white font-extrabold text-3xl">
+          {dayAndMonth}
+        </Text>
+
+        <ProgressBar progress={habitsProgress} />
+
+        <View className={clsx("mt-6", {
+          ["opacity-50"]: isDateInPast
+        })}>
+          {
+            dayInfo?.possibleHabits ?
+            dayInfo?.possibleHabits.map(habit => (
+              <CheckBox 
+                key={habit.id}
+                title={habit.title}
+                checked={completedHabits.includes(habit.id)}
+                disabled={isDateInPast}
+                onPress={() => handleToggleHabits(habit.id)}
+              />
+            ))
+            : <HabitsEmpty />
+          }
+        </View>
+
+        {
+          isDateInPast && 
+          (
+          <Text className="text-white mt-10 text-center">
+            Não é possível editar hábitos de datas passadas.
+          </Text>
+          )
+        }
+      </ScrollView>
+    </View>
+  )
+}
---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,56 @@
+import * as Popover from '@radix-ui/react-popover'
+import clsx from 'clsx'
+import { ProgressBar } from './ProgressBar'
+import dayjs from 'dayjs'
+import { HabitsList } from './HabitsList'
+import { useState } from 'react'
+
+interface HabitDayProps {
+    date: Date
+    defaultCompleted?: number
+    amount?: number
+}
+
+export function HabitDay({ defaultCompleted = 0 , amount = 0, date }: HabitDayProps) {
+    const [completed, setCompleted] = useState(defaultCompleted)
+    
+    const completePercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0
+    
+    const dayAndMonth = dayjs(date).format('DD/MM')
+    const dayOfWeek = dayjs(date).format('dddd')
+
+    function handleCompletedChanged(completed: number) {
+        setCompleted(completed)
+    }
+
+    return (
+        <Popover.Root>
+            <Popover.Trigger 
+                className={clsx('w-10 h-10 border-2 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-300 focus:ring-offset-2 focus:ring-offset-background', {
+                    'bg-zinc-900 border-zinc-800': completePercentage === 0,
+                    'bg-violet-900 border-violet-700': completePercentage > 0 && completePercentage < 20,
+                    'bg-violet-800 border-violet-600': completePercentage >= 20 && completePercentage < 40,
+                    'bg-violet-700 border-violet-500': completePercentage >= 40 && completePercentage < 60,
+                    'bg-violet-600 border-violet-500': completePercentage >= 60 && completePercentage < 80,
+                    'bg-violet-500 border-violet-400': completePercentage >= 80,
+                    
+                })}
+            />
+
+            <Popover.Portal>
+                <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col focus:outline-none focus:ring-2 focus:ring-violet-300 focus:ring-offset-2 focus:ring-offset-background">
+                    <span className="font-semibold text-zinc-400">{dayOfWeek}</span>
+                    <span className="mt-1 font-extrabold leading-tight text-3xl">{dayAndMonth}</span>
+                    
+                    <ProgressBar progress={completePercentage} />
+
+                    <HabitsList date={date} onCompletedChanged={handleCompletedChanged} />
+
+                    <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+                </Popover.Content>
+            </Popover.Portal>
+        
+        
+        </Popover.Root>
+    )
+}
